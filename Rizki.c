c5324bb016807f7f936b00c1bbdd248f2ee2347e
Muhammad Rizki <55613831+Rizki1914370126@users.noreply.github.com>
2019-10-31 12:39:41 +0700

    Add files via upload

---
+++ Rizki.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main()
+{
+    printf("*     * **    * ****    ***   ****  \n");
+    printf("*     * * *   * *   *  *   *  *   * \n");
+    printf("*     * *  *  * ****  ******* ****  \n");
+    printf("*     * *   * * *     *     * *   * \n");
+    printf(" *****  *    ** *     *     * ****  \n");
+    printf("\tWELCOME TO UNPAB\n\n");
+    printf("\tBIODATA PRIBADI SAYA\n\n");
+    printf("NAMA : Muhammad Rizki\n");
+    printf("\NPM : 1914370126\n");
+    printf("\KELAS : REGULER 1D\n");
+    printf("\FAKULTAS : SAINS & TEKNOLOGI\n");
+    printf("\PRODI : SISKOM\n");
+
+
+    int a=800;
+    int b=20;
+    int c;
+    c=a-b;
+    printf("baris 1 -nilai A adalah %d\n",c);
+    c=a+b;
+    printf("baris 2 -nilai B adalah %d\n",c);
+    c=a*b;
+    printf("baris 3 -nilai C adalah %d\n",c);
+    c=a%b;
+    printf("baris 4 -nilai D adalah %d\n",c);
+    c=a/b;
+    printf("baris 5 -nilai E adalah %d\n",c);
+    c=a++;
+    printf("baris 6 -nilai F adalah %d\n",c);
+    c=a--;
+    printf("baris 7 -nilai G adalah %d\n",c);
+    printf("TERIMA KASIH\n");
+    printf("SAMPAI JUMPA KEMBALI");
+    return 0;
+}
